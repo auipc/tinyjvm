a3fe637a057624f1ec2d77792d8e5c8745b663af
auipc <139733387+auipc@users.noreply.github.com>
2023-08-01 01:18:28 -0400

    add preemptive array test

---
+++ tests/Array.java
@@ -0,0 +1,16 @@
+class Array {
+    public static void main(String[] args) {
+	    int[] a = new int[10];
+	    // insert unsorted values into array
+	    a[0] = 5;
+	    a[1] = 2;
+	    a[2] = 7;
+	    a[3] = 3;
+	    a[4] = 9;
+	    a[5] = 1;
+	    a[6] = 6;
+	    a[7] = 4;
+	    a[8] = 8;
+	    a[9] = 0;
+    }
+}
