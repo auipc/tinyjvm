b39e1daef0a25a168351ba3155b39bcfcff15be0
auipc <isaiah.z.abb+1@gmail.com>
2023-09-07 02:41:23 -0400

    Remove bogus endian swapping

--- src/stack.cpp
+++ src/stack.cpp
@@ -2,34 +2,24 @@
 #include <iostream>
 #include <tinyjvm/stack.h>
 
-// Shamelessly stolen from stackoverflow
-// Why doesn't LIBC have this though
-#define htonll(x)                                                              \
-	((1 == htonl(1))                                                           \
-		 ? (x)                                                                 \
-		 : ((uint64_t)htonl((x)&0xFFFFFFFF) << 32) | htonl((x) >> 32))
-#define ntohll(x)                                                              \
-	((1 == ntohl(1))                                                           \
-		 ? (x)                                                                 \
-		 : ((uint64_t)ntohl((x)&0xFFFFFFFF) << 32) | ntohl((x) >> 32))
-
+namespace tinyJVM {
 // FIXME What's with the htonl usage
 // FIXME maybe stack items should be assosciated with primitives
-void Stack::push(int32_t v) { m_stack.push_back(htonl(v)); }
+void Stack::push(int32_t v) { m_stack.push_back(v); }
 
 void Stack::push_64(int64_t v) {
 	// split the 64 bit value into two 32 bit values
 	// and push them onto the stack
 	// FIXME this is probably endianess dependant
-	m_stack.push_back(htonl((int32_t)(v >> 32)));
-	m_stack.push_back(htonl((int32_t)(v & 0xFFFFFFFF)));
+	m_stack.push_back((int32_t)(v >> 32));
+	m_stack.push_back((int32_t)(v & 0xFFFFFFFF));
 }
 
 // FIXME raise error when we try to access stuff from an empty stack
 int32_t Stack::pop() {
 	if (!m_stack.size())
 		throw StackUnderflowException();
-	int32_t v = ntohl(m_stack.back());
+	int32_t v = m_stack.back();
 	m_stack.pop_back();
 	return v;
 }
@@ -48,7 +38,7 @@ int64_t Stack::pop_64() {
 int32_t Stack::peek() {
 	if (!m_stack.size())
 		throw StackUnderflowException();
-	int32_t v = ntohl(m_stack.back());
+	int32_t v = m_stack.back();
 	return v;
 }
 
@@ -56,14 +46,16 @@ int64_t Stack::peek_64() {
 	if (m_stack.size() < 2)
 		throw StackUnderflowException();
 	// FIXME this is probably endianess dependant
-	int64_t v = ntohl(m_stack.at(m_stack.size() - 2));
-	v = (v << 32) | ntohl(m_stack.back());
+	int64_t v = m_stack.at(m_stack.size() - 2);
+	v = (v << 32) | m_stack.back();
 	return v;
 }
 
 void Stack::dump_stack() {
 	std::cout << "Stack dump:\n";
 	for (auto &item : m_stack) {
-		std::cout << "\t" << ntohl(item) << "\n";
+		std::cout << "\t" << item << "\n";
 	}
+}
+
 }
